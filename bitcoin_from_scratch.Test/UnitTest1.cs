9c5959f66503c74801b3a165cc98362fa98cd0ad
ahopkins <ahopkins0929@gmail.com>
2022-10-05 20:27:50 -0400

    Project setup

---
+++ bitcoin_from_scratch.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace bitcoin_from_scratch.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ bitcoin_from_scratch/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace bitcoin_from_scratch
+{
+    public class Program
+    { 
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("New Project");
+        }
+    }
+}
