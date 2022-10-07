2a91a4cfd56c76e0af1aec12bd429661110fa122
ahopkins <ahopkins0929@gmail.com>
2022-10-07 18:05:47 -0400

    Save and load wallets from file

--- bitcoin_from_scratch/Program.cs
+++ bitcoin_from_scratch/Program.cs
@@ -3,7 +3,7 @@
 namespace bitcoin_from_scratch
 {
     public class Program
-    { 
+    {
         public static async Task<int> Main(string[] args)
         {
             return await new CliApplicationBuilder()
