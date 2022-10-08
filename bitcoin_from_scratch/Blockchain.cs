878be1d2c69f30554fac8499a57d42541d3ee104
ahopkins <ahopkins0929@gmail.com>
2022-10-07 21:25:09 -0400

    Basic proof of work, create blockchain

---
+++ bitcoin_from_scratch/Blockchain.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bitcoin_from_scratch
+{
+    public class Blockchain
+    {
+        public Block[] Blocks { get; set; }
+
+        public Blockchain()
+        {
+            var genesisBlock = CreateNewGenesisBlock();
+            Blocks = new Block[] { genesisBlock };
+        }
+
+        private Block CreateNewGenesisBlock()
+        {
+            return new Block("Genesis Block", new byte[0]);
+        }
+    }
+}
---
+++ bitcoin_from_scratch/cli/CreateBlockchainCommand.cs
@@ -0,0 +1,18 @@
+﻿using CliFx;
+using CliFx.Attributes;
+using CliFx.Infrastructure;
+
+namespace bitcoin_from_scratch.cli
+{
+    [Command("create-blockchain")]
+    public class CreateBlockchainCommand : ICommand
+    {
+        public ValueTask ExecuteAsync(IConsole console)
+        {
+            console.Output.WriteLine($"Mining genesis block...");
+            var blockchain = new Blockchain();
+            console.Output.WriteLine("Blockchain created!");
+            return default;
+        }
+    }
+}
